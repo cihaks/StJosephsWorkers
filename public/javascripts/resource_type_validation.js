ee5ce630cbc1d4d2961bd06b3e3ee88459c5c057
Stephen Cihak <cihaks@macintosh-3.(none)>
2010-08-06 12:18:25 -0700

    added check for restricted resource types

---
+++ public/javascripts/resource_type_validation.js
@@ -0,0 +1,18 @@
+
+function validate_resource_type(value) {
+	result = true;
+	if (document.getElementById('restrict_'+value)) {
+		result = confirm("This is a restricted resource type.\n\nAre you sure you want to continue?");
+	}
+	return result;
+}
+
+function check_form() {
+	item = document.getElementById("assigned_resource_resource_type_id");
+	if (item.value != null) {
+	  return validate_resource_type(item.value);
+	} else {
+		return true;
+	}
+}
+	
