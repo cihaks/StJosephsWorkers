6e14cd929529b436b5b3b0d41248cc0169098dd3
Stephen Cihak <cihaks@Macintosh-3.local>
2010-11-17 12:38:36 -0700

    user input model sample

--- app/controllers/reports/new_hires_controller.rb
+++ app/controllers/reports/new_hires_controller.rb
@@ -2,9 +2,10 @@ class Reports::NewHiresController < ApplicationController
     layout 'reports'
 
     def index
-
-
-      @new_hires=Job.find(:all, :conditions=>['start_date >=? and start_date is not null', Time.local(Time.now.year, 1,1)],:order=>'start_date')
+			@test_model = TestModel.new
+			@test_model.start_date = params[:test_model].nil? ? Time.local(Time.now.year, 1,1) : params[:test_model][:start_date]
+			@test_model.end_date = params[:test_model][:end_date] unless params[:test_model].nil? or params[:test_model][:end_date].nil?
+      @new_hires=Job.find(:all, :conditions=>['start_date >=? and start_date is not null', @test_model.start_date],:order=>'start_date')
 
       respond_to do |format|
          format.html   
---
+++ app/models/test_model.rb
@@ -0,0 +1,4 @@
+class TestModel
+	attr_accessor :start_date, :end_date
+	
+end
