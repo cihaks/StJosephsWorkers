6d4e220bb7c62a8be64ab01d2966ad4553ff655c
Stephen Cihak <cihaks@gmail.com>
2010-12-10 00:32:21 -0700

    update admin models to require name attribute; admin controllers now inherit all actions from SecurityController

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,80 +1,80 @@
 class CoursesController < SecurityController
-  layout 'references'
-
-  # GET /Courses
-  # GET /Courses.xml
-  # inherited from SecurityController
-
-  # GET /Courses/1
-  # GET /Courses/1.xml
-  def show
-    @course = Course.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @course }
-    end
-  end
-
-  # GET /Courses/new
-  # GET /Courses/new.xml
-  def new
-    @course = Course.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @course }
-    end
-  end
-
-  # GET /Courses/1/edit
-  def edit
-    @course = Course.find(params[:id])
-  end
-
-  # POST /Courses
-  # POST /Courses.xml
-  def create
-    @course = Course.new(params[:course])
-
-    respond_to do |format|
-      if @course.save
-        flash[:notice] = 'Course was successfully created.'
-        format.html { redirect_to(courses_url) }
-        format.xml  { render :xml => @course, :status => :created, :location => @course }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @course.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /Courses/1
-  # PUT /Courses/1.xml
-  def update
-    @course = Course.find(params[:id])
-
-    respond_to do |format|
-      if @course.update_attributes(params[:course])
-        flash[:notice] = 'Course was successfully updated.'
-        format.html { redirect_to(courses_url) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @course.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /Courses/1
-  # DELETE /Courses/1.xml
-  def destroy
-    @course = Course.find(params[:id])
-    @course.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(courses_url) }
-      format.xml  { head :ok }
-    end
-  end
+  # layout 'references'
+  # 
+  # # GET /Courses
+  # # GET /Courses.xml
+  # # inherited from SecurityController
+  # 
+  # # GET /Courses/1
+  # # GET /Courses/1.xml
+  # def show
+  #   @course = Course.find(params[:id])
+  # 
+  #   respond_to do |format|
+  #     format.html # show.html.erb
+  #     format.xml  { render :xml => @course }
+  #   end
+  # end
+  # 
+  # # GET /Courses/new
+  # # GET /Courses/new.xml
+  # def new
+  #   @course = Course.new
+  # 
+  #   respond_to do |format|
+  #     format.html # new.html.erb
+  #     format.xml  { render :xml => @course }
+  #   end
+  # end
+  # 
+  # # GET /Courses/1/edit
+  # def edit
+  #   @course = Course.find(params[:id])
+  # end
+  # 
+  # # POST /Courses
+  # # POST /Courses.xml
+  # def create
+  #   @course = Course.new(params[:course])
+  # 
+  #   respond_to do |format|
+  #     if @course.save
+  #       flash[:notice] = 'Course was successfully created.'
+  #       format.html { redirect_to(courses_url) }
+  #       format.xml  { render :xml => @course, :status => :created, :location => @course }
+  #     else
+  #       format.html { render :action => "new" }
+  #       format.xml  { render :xml => @course.errors, :status => :unprocessable_entity }
+  #     end
+  #   end
+  # end
+  # 
+  # # PUT /Courses/1
+  # # PUT /Courses/1.xml
+  # def update
+  #   @course = Course.find(params[:id])
+  # 
+  #   respond_to do |format|
+  #     if @course.update_attributes(params[:course])
+  #       flash[:notice] = 'Course was successfully updated.'
+  #       format.html { redirect_to(courses_url) }
+  #       format.xml  { head :ok }
+  #     else
+  #       format.html { render :action => "edit" }
+  #       format.xml  { render :xml => @course.errors, :status => :unprocessable_entity }
+  #     end
+  #   end
+  # end
+  # 
+  # # DELETE /Courses/1
+  # # DELETE /Courses/1.xml
+  # def destroy
+  #   @course = Course.find(params[:id])
+  #   @course.destroy
+  # 
+  #   respond_to do |format|
+  #     format.html { redirect_to(courses_url) }
+  #     format.xml  { head :ok }
+  #   end
+  # end
 end
