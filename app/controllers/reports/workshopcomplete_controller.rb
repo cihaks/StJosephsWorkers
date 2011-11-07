920b938b3fc7e585d43a9f61cf51d6f33c101d2e
Stephen Cihak <cihaks@gmail.com>
2011-11-06 21:08:08 -0700

    fixed error from new scheduled course object

--- app/controllers/reports/workshopcomplete_controller.rb
+++ app/controllers/reports/workshopcomplete_controller.rb
@@ -2,8 +2,8 @@ class Reports::WorkshopcompleteController < ApplicationController
   layout 'reports'
   
   def index
-  @workshopcomplete=RegisteredClass.find(:all,:conditions=>["completed=0 and class_date>=?",Time.local(Time.now.year,1,1)])
-  @workshopcomplete_months=@workshopcomplete.group_by { |m| m.class_date.beginning_of_month}
+  @workshopcomplete=RegisteredClass.find(:all,:include=>:scheduled_course, :conditions=>["completed=0 and scheduled_courses.course_date>=?",Time.local(Time.now.year,1,1)])
+  @workshopcomplete_months=@workshopcomplete.group_by { |m| m.scheduled_course.course_date.beginning_of_month}
 
  
     respond_to do |format|
