9185205ab5271253cd6741c22f54955aebc59218
Stephen Cihak <cihaks@gmail.com>
2012-07-21 09:49:30 -0700

    reverted scheduledcourse changes

--- app/controllers/scheduled_courses_controller.rb
+++ app/controllers/scheduled_courses_controller.rb
@@ -40,17 +40,44 @@ class ScheduledCoursesController < SecurityController
   # POST /scheduled_courses
   # POST /scheduled_courses.xml
   # def create
-    # @scheduled_course = ScheduledCourse.new(params[:scheduled_course])
-    #   
-    # respond_to do |format|
-    #   if @scheduled_course.save
-    #     format.html { redirect_to(@scheduled_course, :notice => 'ScheduledCourse was successfully created.') }
-    #     format.xml  { render :xml => @scheduled_course, :status => :created, :location => @scheduled_course }
-    #   else
-    #     format.html { render :action => "new" }
-    #     format.xml  { render :xml => @scheduled_course.errors, :status => :unprocessable_entity }
-    #   end
-    # end
+  #   @scheduled_course = ScheduledCourse.new(params[:scheduled_course])
+  # 		
+  # 		# sample params {"start_date"=>"07/30/2012", 
+  # 		# 								"occurrences"=>"5", 
+  # 		# 								"scheduled_course"=>{"location"=>"", "teacher"=>"", "capacity"=>"4", "course_id"=>"3"}, 
+  # 		# 								"weekly_on"=>"Monday", 
+  # 		# 								"frequency"=>"weekly", 
+  # 		# 								"monthly_on"=>"1", 
+  # 		# 								"start_time"=>{"minute"=>"16", "hour"=>"12"}}
+  # 		
+  # 		#determine if weekly or monthly
+  # 		if params[:frequency] == 'weekly'
+  # 			#if week day, is startdate on same day of the week
+  # 			
+  # 		elsif params[:frequency] == 'biweekly'
+  # 			#if week day, is startdate on same day of the week
+  # 			
+  # 		elsif params[:frequency] == 'monthly'
+  # 			#if month day, is start date a match
+  # 			
+  # 		end
+  # 
+  # 		#determine frequency and create as many instances as needed
+  # 			#combine date and start time
+  # 			#suggestion:  create array of dates with start time and loop through to create all instances
+  # 			
+  # 		#save all instances within a transaction
+  #     # e.g. ScheduledCourse.transaction do ... end
+  # 
+  #   respond_to do |format|
+  #     if @scheduled_course.save
+  #       format.html { redirect_to(@scheduled_course, :notice => 'ScheduledCourse was successfully created.') }
+  #       format.xml  { render :xml => @scheduled_course, :status => :created, :location => @scheduled_course }
+  #     else
+  #       format.html { render :action => "new" }
+  #       format.xml  { render :xml => @scheduled_course.errors, :status => :unprocessable_entity }
+  #     end
+  #   end
   # end
   # 
   # # PUT /scheduled_courses/1
